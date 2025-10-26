a53b0a5b5a0c7764d6814e76aacf46527a5209da
bhargavi-durga <bhargavi.durga2019@gmail.com>
2025-10-26 15:18:52 +0100

    Added the utility method for patientId generation

---
+++ src/main/java/com/flmhospitals/utils/PatientIdGenerator.java
@@ -0,0 +1,36 @@
+package com.flmhospitals.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Component;
+
+import com.flmhospitals.dao.PatientRepository;
+
+@Component
+public class PatientIdGenerator {
+
+    private final PatientRepository patientRepository;
+
+    public PatientIdGenerator(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public String generateNextPatientId() {
+        String prefix = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String lastId = patientRepository.findLastPatientId();
+
+        int nextNumber = 1;
+
+        if (lastId != null) {
+            
+            String numberPart = lastId.substring(8);
+            nextNumber = Integer.parseInt(numberPart) + 1;
+        }
+
+       
+        String suffix = String.format("%06d", nextNumber);
+
+        return prefix + suffix;
+    }
+}
